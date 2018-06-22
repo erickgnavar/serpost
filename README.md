b1720a0787247ba068107af27674fcd9b6cffbd6
Erick Navarro <erick@navarro.io>
2018-06-21 20:08:26 -0700

    Replace obsolete Serpost url
    Now use a new Serpost service available without authentication

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Serpost CLI
 [![Build Status](https://travis-ci.org/erickgnavar/serpost.svg?branch=master)](https://travis-ci.org/erickgnavar/serpost)
 
-CLI tool for search tracking number in SERPOST page
+CLI tool for search tracking number in SERPOST page, works in python 2 and python 3
 
 ### Install
 
@@ -29,3 +29,9 @@ Tracking number: 123
 15/08/2014 03:00 | ENVIO INGRESA A LA OFICINA CENTRAL DE Peru                                                          
 15/08/2014 03:08 | DISPONIBLE PARA SALIR A United States of America (the)                                              
 ```
+
+### TODO:
+
+- [ ] Add tests
+- [ ] Better error handling
+- [ ] Add a logger
--- serpost/serpost.py
+++ serpost/serpost.py
@@ -1,70 +1,72 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime
-import logging
+import json
 import sys
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 
-
 if sys.version_info >= (3, 0):
-    from urllib.request import urlopen
-    from urllib.parse import urlencode
+    from urllib.request import urlopen, Request
 else:
-    from urllib2 import urlopen
-    from urllib import urlencode
+    from urllib2 import urlopen, Request
 
 
-logger = logging.getLogger(__name__)
-logging.basicConfig()
+TRACKING_URL = 'http://clientes.serpost.com.pe/prj_online/Web_Busqueda.aspx/Consultar_Tracking'
+TRACKING_DETAIL_URL = 'http://clientes.serpost.com.pe/prj_online/Web_Busqueda.aspx/Consultar_Tracking_Detalle'
 
 
-url = 'http://clientes.serpost.com.pe/prj_tracking/seguimientolinea.aspx'
-
-DATE_FORMAT_1 = '%d/%m/%Y %I:%M'
-DATE_FORMAT_2 = '%d/%m/%Y %H:%M'
-
-
-def prepare_payload():
-    response = urlopen(url)
-    soup = BeautifulSoup(response.read(), 'lxml')
-    form = soup.find('form')
-    data = {}
-    for input_tag in form.find_all('input'):
-        data[input_tag['name']] = input_tag.get('value')
-    return data
+def _make_request(url, payload):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    req = Request(url, headers=headers)
+    data = json.dumps(payload).encode('utf8')
+    response = urlopen(req, data=data)
+    return json.loads(response.read())
 
 
-def format_date(date_str):
-    if 'a.m.' in date_str or 'p.m.' in date_str:
-        date_str = date_str.strip()[:16]
-        return datetime.strptime(date_str, DATE_FORMAT_1)
-    elif '-' in date_str:
-        date_str = date_str.replace('-', '')
-        return datetime.strptime(date_str, DATE_FORMAT_2)
-    else:
-        logger.warn('Format not found for: %s', date_str)
-        return None
+def _process_detail(raw):
+    soup = BeautifulSoup(raw, 'lxml')
+    rows = []
+    for row in soup.find_all('tr'):
+        cells = []
+        for td in row.find_all('td'):
+            cells.append(td.text.strip())
+        rows.append(cells)
+
+    res = []
+    for row in rows:
+        place, date_str, message = row
+        data = datetime.strptime(date_str, '%d/%m/%Y')
+        res.append({
+            'date': data,
+            'message': message,
+            'place': place,
+        })
+    return sorted(res, key=lambda x: x['date'])
 
 
 def query_tracking_code(tracking_code):
-    payload = prepare_payload()
-    payload['txtTracking'] = tracking_code
-    response = urlopen(url, urlencode(payload).encode('utf-8'))
-    soup = BeautifulSoup(response.read(), 'lxml')
-    table = soup.find(id='gvSegEnvio')
-    if table is None:
+    """
+    Given a tracking_code return a list of events related the tracking code
+    """
+    payload = {
+        'Anio': datetime.now().year,
+        'Tracking': tracking_code,
+    }
+    response = _make_request(TRACKING_URL, payload)
+
+    if not response['d']:
         return []
-    result = []
-    for tr in table.find_all('tr'):
-        tds = tr.find_all('td')
-        if not tds or len(tds) != 2:
-            continue
-        date_object = format_date(tds[0].text)
-        if date_object is not None:
-            result.append({
-                'date': date_object,
-                'message': tds[1].text
-            })
-    return sorted(result, key=lambda x: x['date'])
+
+    data = response['d'][0]
+
+    destination = data['RetornoCadena6']
+    payload.update({
+        'Destino': destination,
+    })
+
+    response = _make_request(TRACKING_DETAIL_URL, payload)
+    return _process_detail(response['d']['ResulQuery'])
